ea56815d60dd6037c84605e0f053cea431cff919
DavidM <d.a.manrique.negrin@tue.nl>
2026-03-23 14:49:00 +0100

    New environment created

--- env_api/Scripts/activate.bat
+++ env_api/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\20181234\PersonalDave\API_tests\env_api
+set VIRTUAL_ENV=D:\Git_folders\OrchTwinLang\env_api
 
 if not defined PROMPT set PROMPT=$P$G
 
